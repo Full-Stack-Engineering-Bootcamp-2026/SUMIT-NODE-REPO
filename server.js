7d43c9dc3a825b5410439f4b7aee1985888a0788
sumit haverikar <sumeet.haverikar@mindbowser.com>
2026-03-18 17:55:24 +0530

    fix:updated port number

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200 ,{ 'Content-Type': 'text/html' });
 
-    res.end(` <h1>Name: Sumit Haverikar</h1>
+    res.end(`<h1>Name: Sumit Haverikar</h1>
 <p>Today's Date: ${new Date().toLocaleString()}</p>`);
+
 });
 
 server.listen(3100, () => {
