f24e125832e3873e8acad1e2fd7dc0b6933c2db5
sumit haverikar <sumeet.haverikar@mindbowser.com>
2026-03-19 17:27:22 +0530

    feature: completed Day-3 Node assignment

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-const http = require('http');
+const http = require("http");
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    console.log(req.method, req.url);
 
-    res.end(` <h1>Name: Sumit Haverikar</h1>
-<p>Today's Date: ${new Date().toLocaleString()}</p>`);
+    // home (GET req )
+    if (req.url === "/" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Home Page</h1> <a href = '/about'> About</a> <br> <a href =`/time`>Time</a>");
+    }
+
+
+    // about 
+    else if (req.url === "/about" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1><p>Sumit Haverikar </p>")
+    }
+    //rediredct
+    else if (req.url === "/redirect") {
+        res.writeHead(302, { Location: "/" });
+        res.end();
+    }
+
+    //home (post request )
+    else if (req.url === "/" && req.method === "POST") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Home page</h1> <a href = '/about'>About</a>");
+    }
+
+    // TIME - GET (JSON)
+    else if (req.url === "/time" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
+            time: new Date().toLocaleString()
+        }));
+    }
+
+    // 404 
+    else {
+        res.writeHead(404, { "content-Type": "text/html" });
+        res.end("<h1> 404  Page Not Found </h1>")
+    }
 });
 
-server.listen(3100, () => {
-    console.log(`Server is running on http://localhost:${3100}`);
-});
+server.listen(3000, () => {
+    console.log(`server running on http://localhost:${3000}`);
+})
