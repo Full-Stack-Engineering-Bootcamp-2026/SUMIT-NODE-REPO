d0343729a15af58356525ac645787b5b89a140a5
sumit haverikar <sumeet.haverikar@mindbowser.com>
2026-03-29 22:40:37 +0530

    created a route to handle get and post request

---
+++ Day-8/routes/usersRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    const { name, age } = req.query;
+
+    res.json({
+        message: "GET request successful",
+        data: {
+            name,
+            age,
+        },
+    });
+});
+
+
+router.post("/", (req, res) => {
+    const { name, email } = req.body;
+
+    res.json({
+        message: "POST request successful",
+        data: {
+            name,
+            email,
+        },
+    });
+});
+
+module.exports = router;
